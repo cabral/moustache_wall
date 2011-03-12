25e7529dbeb7284761b366f13ea7518c22b4e283
Diogo Biazus <diogob@gmail.com>
2011-03-12 16:22:26 -0300

    adding file upload to form

--- test/functional/moustaches_controller_test.rb
+++ test/functional/moustaches_controller_test.rb
@@ -15,6 +15,7 @@ class MoustachesControllerTest < ActionController::TestCase
     assert assigns(:moustache).new_record?
     assert_select "form#new_moustache" do
       assert_select "input#moustache_name[name='moustache[name]']"
+      assert_select "input#moustache_image[name='moustache[image]']"
       assert_select "input[type='submit']"
     end
   end
