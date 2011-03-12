82aced95deeee3808363771fcf2857e39cdd4ff5
Diogo Biazus <diogob@gmail.com>
2011-03-12 13:03:12 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MustacheWall::Application.config.secret_token = '0110d92ce6b8697c878b5b947edfd8c12cd62146c318ae631455c95c996de75ec50502e3bc61232dfc65798e32788c459488a8ecd6a1e207ab5744003b671662'
